9bd14663f9643fd22a18c59b08cbe019f1e84426
vedamshpawar <vedamshpawar@gmail.com>
2025-12-31 17:53:40 +0530

    setted all gone through all files

--- myproject_angular/src/app/app.ts
+++ myproject_angular/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('myproject_angular');
+  protected readonly title = signal('TODO-List');
 }
